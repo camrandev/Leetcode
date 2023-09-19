17c9f3f0ff08646424986ceec46609cb57560bd6
Camran Rynowecer <Crynowecer11@gmail.com>
2023-09-19 17:40:33 -0400

    Time: 42 ms (69.33%), Space: 16.2 MB (77.25%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -1,66 +1,41 @@
+#move across the two lists in tandem, creating a new list which is the two input lists combineded, sorted in non-ascending order
+
+#use a dummy node to start the list off
+#create another variable to track the tail, so we know where to insert
+
+#compare the values at the current nodes in each list, while both lists exist
+#add the lesser value to the merged list of list1 and list2 -> list1 and list2 point to a node each
+#when an update is performed, update list1/list2 to point to its next property
+
+#if anything remains, add it to the end of the list
+
+#return the merged_list.next value, as merged_list variable will be pointing to the dummy head node. 
+
 # Definition for singly-linked list.
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
-
-
-#track the new list
-#perform the main merge
-#add any remianing to the end of the new list
-#return the head node of the new list
-
-#psuedocode
-
-#setup problem
-#declare a variable to hold a dummy node
-#keep track of the tail, so we know where to add any values
-
-#main merge
-#while both lists exist
-    #if val of list1 > the value of list2
-        #set the tails next to the list 2 node
-        #set the list2 pointer to its next value    
-    #else:
-        #set the tails next pointer to the list1 node
-        #set the list 1 pointer to its next value
-    #set the tail to its own next pointer
-
-#merge any remianing values
-    #don need to track the tail after this step
-    #if list1 exists -> set the tails next to l1
-    #set tails next to list2
-
-#return the merged list
-
-# list1 = [1,2,4], list2 = [1,3,4]        
-
-#l1 = 2
-#l2 = 1
-
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
         merged_list = ListNode()
-        tail = merged_list # 1 2*
+        tail = merged_list
 
-        #do the main merge while we have two lists
-        while list1 and list2:
-            if list1.val > list2.val:
-                tail.next = list2
-                list2 = list2.next
-            else:
+        #perform the main merge -> dont forget to update the pointers for the list nodes
+        while(list1 and list2):
+            if list1.val <= list2.val:
                 tail.next = list1
                 list1 = list1.next
-            tail = tail.next
-        
-        #merge the remaining
+            else:
+                tail.next = list2
+                list2 = list2.next
+            tail = tail.next #update the tail, so we know where to insert
+
+        #merge any remaining poritions
         if list1:
             tail.next = list1
-        else:
+        if list2:
             tail.next = list2
         
-        #must return merged_list.next, as merged_list itself is a dummy node
-        return merged_list.next
-        
-
+        return merged_list.next #merged_list.next points to the value that we want
         
