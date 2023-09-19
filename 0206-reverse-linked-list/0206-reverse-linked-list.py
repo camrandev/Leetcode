6efe1b45d2edbe59ba772ad91d2b728bd0ca77b7
Camran Rynowecer <Crynowecer11@gmail.com>
2023-09-19 17:31:47 -0400

    Time: 50 ms (16.03%), Space: 17.9 MB (69.79%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.py
+++ 0206-reverse-linked-list/0206-reverse-linked-list.py
@@ -1,3 +1,6 @@
+#move across the linked list, setting the next pointer to the previous node in the list if it exists
+
+
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
@@ -5,17 +8,20 @@
 #         self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        #track curr, prev
-        curr, prev = head, None
+        #track the current and the previous
+        curr = head
+        prev = None
 
         while curr:
-            #save next
+            #save the next value
             nxt = curr.next
-            #set next
+            #update the next value -> previous value
             curr.next = prev
-            #update pointers (curr->nxt, prev->curr)
-            curr, prev = nxt, curr
+            #update the previous value to the current value
+            prev = curr
+            #update the current to the saved next value
+            curr = nxt
         
-        #prev -> because previous will now be pointing to the new head
+        #return prev, as prev will be pointing to the new head at the end of the algorithm
         return prev
         
