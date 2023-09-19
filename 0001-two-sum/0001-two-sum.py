6d8392d50061528c57b76d9a9f5d4ceea2f3b648
Camran Rynowecer <Crynowecer11@gmail.com>
2023-09-19 14:44:46 -0400

    Time: 34 ms (91.25%), Space: 14.1 MB (60.54%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,34 @@
+#psuedocode
+#declare a map to store which numbers have been looked at
+
+#iterate over the nums
+    #calculate the complement - the value that which added to the current num = the target
+    #if the comp exists in the map
+        #return the index of the comp + the current index
+    #otherwise
+        #add the comp to the map as a key, storing its index as a value
+    
+    #nothing else is required, as we know we will always have a working solution according to the prompt
+
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        hashmap = {}
+        #iterate over the numbers, accessing both the index and number
+        for i, num in enumerate(nums):
+            #calculate the complement
+            comp = target - num
+            #check if the complement exists in the hashmap
+            if comp in hashmap:
+                #if it does, return the value at the complement in the [hashmap, index]
+                return [hashmap[comp], i]
+            else:
+                #if comp not in hm: add the current number as a key, with index as value
+                hashmap[num] = i
+
+        
