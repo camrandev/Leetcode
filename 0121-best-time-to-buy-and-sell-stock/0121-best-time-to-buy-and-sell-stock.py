d0500a087b336eb2baf0672333ecaaf6ab0f399f
Camran Rynowecer <Crynowecer11@gmail.com>
2023-09-19 14:55:31 -0400

    Time: 868 ms (56.76%), Space: 27.3 MB (88.64%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,34 +1,30 @@
-#overview: find the most profitable potential stock trade. At a very high level, we need to move acorss the array, finding the lowest values, and finding the highest value after that, if we find a value lower than the current low, set the low to the new low
+#set pointers to track the buy and sell price
+#set variable to track the current max profit
+
+#move the pointers across prices to deterimine the max price
+
+#price determination algo:
+#with b starting at 0 and s starting at 1
+    #check if s is greater than b
+        #if it is, calculcate the profit of buying at b and selling at s
+        #if that profit is greater than the current max, update it
+    #if s is not greater than b, you have found a dip
+        #move b over to where s is
+    #move s ahead by 1
+        #this works, because anytime b pointer is > s pointer, you have found a potential buy point, and the algorithm will then check the profit for each possible trade after this against the current max profit
 
 
-#approach - this works because the tracked profit tracks the actual max profit, pointers only care if an individual trade is profitable
-#set pointers to b and s
-#set current max profit to 0
-
-#want to move the sell point all the way across the list, the buy point will be updated as apprpriate
-
-#check if a current trade is a profitable one
-#it is profitable if the value at s is greater than the value at b
-    #if it is a profitable trade, then check if it is more profitable than current max and update accordingly
-
-#if not a profitable trade
-    #move b pointer to the current position of the sell pointer, as it indicates we have found a new low point
-
-#update the b pointer one space
-
-#return the profit
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         b, s = 0, 1
         max_p = 0
 
-        while s < len(prices):
-            if prices[b] < prices[s]:
+        while (s < len(prices)):
+            if prices[s] > prices[b]:
                 profit = prices[s] - prices[b]
-                max_p = max(max_p, profit)
+                max_p = max(profit, max_p)
             else:
                 b = s
             s += 1
-        
-        return max_p
-        
+
+        return max_p        
