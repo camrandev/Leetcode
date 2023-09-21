cb2f6026d07b20a3d7e9362fafa226d910e635e6
Camran Rynowecer <Crynowecer11@gmail.com>
2023-09-21 17:28:39 -0400

    Time: 1861 ms (10.89%), Space: 27.7 MB (63.43%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -1,18 +1,23 @@
 #take the last k elements, move them to the front of the array
 
+#bruteforce
 #iterate up to k
     #on each iteration
     #save + pop the last value
     #insert it at the front
+
+#optimal
 
 class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
+        #bruteforce
         index = 0
         while index < k:
             val = nums.pop()
             nums.insert(0, val)
             index+=1
+        #optimal
         
