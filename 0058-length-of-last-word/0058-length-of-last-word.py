1f54bdf30aaff2cfd49cd7dea8051adf3ed5a819
Camran Rynowecer <Crynowecer11@gmail.com>
2023-09-22 13:45:17 -0400

    Time: 30 ms (93.89%), Space: 16.4 MB (48.17%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.py
+++ 0058-length-of-last-word/0058-length-of-last-word.py
@@ -1,3 +1,7 @@
+#split the string into a list of words
+#python split method takes care of the trimming for us
+#return the length of the last word
+
 class Solution:
     def lengthOfLastWord(self, s: str) -> int:
         words = s.split()
