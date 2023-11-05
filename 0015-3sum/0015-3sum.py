d90262a69133ada441a05ff3db2768d108f002dd
Camran Rynowecer <Crynowecer11@gmail.com>
2023-11-05 14:12:23 -0500

    Time: 1983 ms (26.79%), Space: 21.6 MB (7.62%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,15 +1,16 @@
 #sort the array
-#declare variable to store an output list
+#declare variable to store an output set
 #iterate over the array
     #set a pointer, i, to track where we are in the outer loop
     #on each iteration
     #declare pointers l and r
     #declare a sum to be values at i + l + r
     #if the sum is less than 0, increment the l pointer
-    #if the sum is greater than 0, break the loop
-    #if the sum is equal to 0, add [i, l, r ] to output array
+    #if the sum is greater than 0, decrement r pointer
+    #if the sum is equal to 0, add a tuple 
+    #containing the values at the i, l, r to the out set
 
-#return the output array once above has finished
+#return the output set once above has finished (can perform conversion to list if needed)
 
 
 class Solution:
@@ -30,7 +31,8 @@ class Solution:
                 else:
                     out.add((sorted_nums[i], sorted_nums[l], sorted_nums[r]))
                     l += 1
-        
-        return out
+                    r -= 1
+        #optionally, convert to set of tuples to a list of list before returning it
+        return [list(t) for t in out]
 
         
